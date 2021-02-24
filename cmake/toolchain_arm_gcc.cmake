eb6b12662f172e65fc799179957cada25e8fa9f4
kabirz <jxwazxzhp@126.com>
2021-02-24 23:11:57 +0800

    add freertos build

--- cmake/toolchain_arm_gcc.cmake
+++ cmake/toolchain_arm_gcc.cmake
@@ -5,9 +5,10 @@ set(CMAKE_C_COMPILER ${CROSS_COMPILE}-gcc)
 set(CMAKE_ASM_COMPILER ${CROSS_COMPILE}-gcc)
 set(CMAKE_CXX_COMPILER ${CROSS_COMPILE}-g++)
 set(CMAKE_OBJCOPY ${CROSS_COMPILE}-objcopy CACHE INTERNAL "objcopy tool")
+set(CMAKE_OBJDUMP ${CROSS_COMPILE}-objdump CACHE INTERNAL "objdump tool")
 set(CMAKE_C_FLAGS "-mthumb -fno-builtin -ffunction-sections -fdata-sections -Wno-unused-but-set-variable -Wall -Wunused-parameter -Wundef -fno-omit-frame-pointer -fno-stack-protector")
 set(CMAKE_ASM_FLAGS "-mthumb -mcpu=cortex-m3 -x assembler-with-cpp")
-set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs -mabi=aapcs -Wl,--gc-sections -mthumb -static -Xlinker -Map=${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}.map")
+set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -lc -lgcc -lnosys --specs=nosys.specs -mabi=aapcs -Wl,--gc-sections -mthumb -static -Xlinker -Map=${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}.map")
 
 
 if (NOT BOARD)
--- examples/blinky/main.c
+++ examples/blinky/main.c
@@ -1,9 +1,40 @@
-#include <stdlib.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <FreeRTOS.h>
+#include <queue.h>
+
+/* Set STM32 to 72 MHz. */
+static void clock_setup(void)
+{
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+
+	/* Enable GPIOB and GPIOC clock. */
+	rcc_periph_clock_enable(RCC_GPIOB);
+}
+
+static void gpio_setup(void)
+{
+
+	/* Set GPIO7 (in GPIO port B) to 'output push-pull'. */
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO7);
+}
 
 
 int main(void)
 {
+	QueueHandle_t queue;
+	clock_setup();
+	gpio_setup();
+
+	queue = xQueueCreate(2, sizeof(uint32_t));
+
+	while (1) {
+		gpio_toggle(GPIOB, GPIO7);	/* LED on/off */
+
+		for (int i = 0; i < 1000000; i++)	/* Wait a bit. */
+			__asm__ __volatile__ ("nop");
+	}
+
 	return 0;
 }
