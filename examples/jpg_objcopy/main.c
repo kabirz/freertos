9e5f792003b87326c40173ee8e2f119a79e97eb2
xul1x <lix.w.xu@intel.com>
2021-09-26 11:00:25 +0800

    update rename symbol

--- examples/jpg_objcopy/main.c
+++ examples/jpg_objcopy/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdint.h>
 
-extern uint8_t _binary_pic_jpg_end[];
-extern uint8_t _binary_pic_jpg_start[];
+extern uint8_t image_end[];
+extern uint8_t image_start[];
 
 int main(int argc, char *argv[])
 {
-	size_t len = _binary_pic_jpg_end - _binary_pic_jpg_start;
+	size_t len = image_end - image_start;
 
-	printf("start:\t%p\n", _binary_pic_jpg_start);
-	printf("end:\t%p\n", _binary_pic_jpg_end);
-	printf("size:\t%lu\n", len);
+	printf("start:\t%p\n", image_start);
+	printf("end:\t%p\n", image_end);
+	printf("size:\t%#010x\n", len);
 	return 0;
 }
